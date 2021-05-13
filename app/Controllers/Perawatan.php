a050718301c2f07ae99099e4c6556be12e1b07c3
Deddy Romnan <romnandeddy@gmail.com>
2021-05-13 22:50:10 +0700

    create methods

--- app/Controllers/Perawatan.php
+++ app/Controllers/Perawatan.php
@@ -2,10 +2,31 @@
 
 namespace App\Controllers;
 
+use App\Models\PerawatanModel;
+
 class Perawatan extends BaseController
 {
+    protected $perawatanModel;
+
+    public function __construct()
+    {
+        $this->perawatanModel = new PerawatanModel();
+    }
+
     public function index()
     {
-        return view('perawatan/index.php');
+        $data = [
+            'title' => 'Daftar Perawatan',
+            'perawatan' => $this->perawatanModel->findAll()
+        ];
+        return view('perawatan/index', $data);
+    }
+
+    public function create()
+    {
+        $data = [
+            'title' => 'Registrasi Perawatan'
+        ];
+        return view('perawatan/create', $data);
     }
 }
