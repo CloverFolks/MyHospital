7f534ac4c7e550f08667a0bcf8416eaa192e9d92
Deddy Romnan <romnandeddy@gmail.com>
2021-05-13 22:50:50 +0700

    add perawatan list functionality

--- app/Views/perawatan/index.php
+++ app/Views/perawatan/index.php
@@ -1,5 +1,44 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<h1>Hello world!</h1>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class=""><?= $title; ?></h1>
+        </div>
+        <div class="col">
+            <a href="/perawatan/create" class="btn btn-primary mt-3">Tambah Registrasi Perawatan</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">No. Registrasi</th>
+                        <th scope="col">Poliklinik</th>
+                        <th scope="col">Tanggal Masuk</th>
+                        <th scope="col">Tanggal Keluar</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1 ?>
+                    <?php foreach ($perawatan as $p) : ?>
+                        <tr>
+                            <th scope="row"><?= $i++; ?></th>
+                            <td><?= $p['no_registrasi']; ?></td>
+                            <td><?= $p['poliklinik']; ?></td>
+                            <td><?= $p['tgl_masuk']; ?></td>
+                            <td><?= $p['tgl_keluar']; ?></td>
+                            <td><a href="<?= base_url('/perawatan/' . $p['id']); ?>" class="btn btn-success">Detail</a></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 <?= $this->endSection(); ?>
