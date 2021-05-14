e8ece9171e034498a6107fc75cc6628682166219
Deddy Romnan <romnandeddy@gmail.com>
2021-05-14 22:05:52 +0700

    add detail view

---
+++ app/Views/perawatan/detail.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Test</h1>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
