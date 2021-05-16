a6da69788d9581ba3c96ce3fd87e658cd152888b
Deddy Romnan <romnandeddy@gmail.com>
2021-05-16 21:30:00 +0700

    add method to get fresh no reg and add allowed fields

--- app/Models/PerawatanModel.php
+++ app/Models/PerawatanModel.php
@@ -7,6 +7,7 @@ use CodeIgniter\Model;
 class PerawatanModel extends Model
 {
     protected $table = 'registrasi_perawatan';
+    protected $allowedFields = ['no_registrasi', 'tgl_masuk', 'tgl_keluar', 'poliklinik', 'id_dokter', 'id_pasien'];
 
     public function getPerawatan($id = false)
     {
@@ -15,6 +16,17 @@ class PerawatanModel extends Model
         } else {
             return $this->where(['id' => $id])->first();
         }
+    }
+
+    public function getFreshNoRegistrasi()
+    {
+        do {
+            $noRegistrasi = "";
+            for ($i = 0; $i < 10; $i++) {
+                $noRegistrasi .= mt_rand(0, 9);
+            }
+        } while ($this->where(['no_registrasi' => $noRegistrasi])->first());
+        return $noRegistrasi;
     }
 
     public function search($keyword)
