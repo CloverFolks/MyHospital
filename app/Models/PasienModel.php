fda98e0a4f8aef4f1b2e94761ad28698c04a533f
mfebrindafian <mfebrindafian16@gmail.com>
2021-05-17 05:40:40 +0700

    menambah model pasien

--- app/Models/PasienModel.php
+++ app/Models/PasienModel.php
@@ -7,6 +7,7 @@ use CodeIgniter\Model;
 class PasienModel extends Model
 {
     protected $table = 'pasien';
+    protected $allowedFields = ['nik', 'image_profile', 'no_rekam_medis', 'nama_pasien', 'pekerjaan', 'no_hp', 'alamat', 'jenis_kelamin', 'golongan_darah', 'tgl_lahir'];
 
     public function getPasien($id = false)
     {
@@ -15,5 +16,20 @@ class PasienModel extends Model
         } else {
             return $this->where(['id' => $id])->first();
         }
+    }
+
+    public function search($keyword)
+    {
+        return $this
+            ->table('dokter')
+            ->like('nik', $keyword)
+            ->orLike('no_rekam_medis', $keyword)
+            ->orLike('nama_pasien', $keyword)
+            ->orLike('pekerjaan', $keyword)
+            ->orLike('no_hp', $keyword)
+            ->orLike('jenis_kelamin', $keyword)
+            ->orLike('alamat', $keyword)
+            ->orLike('golongan_darah', $keyword)
+            ->orLike('status_menikah', $keyword);
     }
 }
