69410b394c50edcb32f8559219c1b4cc4f913c00
mfebrindafian <mfebrindafian16@gmail.com>
2021-05-17 03:29:44 +0700

    membuat model dokter

--- app/Models/DokterModel.php
+++ app/Models/DokterModel.php
@@ -7,6 +7,7 @@ use CodeIgniter\Model;
 class DokterModel extends Model
 {
     protected $table = 'dokter';
+    protected $allowedFields = ['nik', 'nip', 'nama', 'email', 'alamat', 'jenis_kelamin', 'image_profile', 'izin_praktek', 'tgl_mulai_bekerja', 'no_hp'];
 
     public function getDokter($id = false)
     {
@@ -15,5 +16,19 @@ class DokterModel extends Model
         } else {
             return $this->where(['id' => $id])->first();
         }
+    }
+
+    public function search($keyword)
+    {
+        return $this
+            ->table('dokter')
+            ->like('nik', $keyword)
+            ->orLike('nip', $keyword)
+            ->orLike('nama', $keyword)
+            ->orLike('email', $keyword)
+            ->orLike('alamat', $keyword)
+            ->orLike('izin_praktek', $keyword)
+            ->orLike('tgl_mulai_bekerja', $keyword)
+            ->orLike('no_hp', $keyword);
     }
 }
