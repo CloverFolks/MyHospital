db0170ba4b1e19eb4673d36c0b4b2ee9e6e1d0ee
Deddy Romnan <romnandeddy@gmail.com>
2021-05-17 16:21:55 +0700

    reset seeder

--- app/Database/Seeds/ObatSeeder.php
+++ app/Database/Seeds/ObatSeeder.php
@@ -12,17 +12,17 @@ class ObatSeeder extends Seeder
 {
     public function run()
     {
-        $faker = \Faker\Factory::create('id_ID');
-        $suffix = array('an', 'at', 'ol', 'or', 'in', 'line', 'ida', 'nia', 'en', 'tyl', 'am');
+        // $faker = \Faker\Factory::create('id_ID');
+        // $suffix = array('an', 'at', 'ol', 'or', 'in', 'line', 'ida', 'nia', 'en', 'tyl', 'am');
 
-        for ($i = 0; $i < 20; $i++) {
-            $nama = $faker->words($faker->numberBetween(1, 2), true) . $faker->randomElement($suffix);
-            $data = [
-                'kode' => $faker->unique()->ean13(),
-                'nama_obat' => ucwords($nama),
-                'produsen' => $faker->company(),
-            ];
-            $this->db->table('obat')->insert($data);
-        }
+        // for ($i = 0; $i < 20; $i++) {
+        //     $nama = $faker->words($faker->numberBetween(1, 2), true) . $faker->randomElement($suffix);
+        //     $data = [
+        //         'kode' => $faker->unique()->ean13(),
+        //         'nama_obat' => ucwords($nama),
+        //         'produsen' => $faker->company(),
+        //     ];
+        //     $this->db->table('obat')->insert($data);
+        // }
     }
 }
