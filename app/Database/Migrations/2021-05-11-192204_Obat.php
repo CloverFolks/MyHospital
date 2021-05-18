7a8239e0f3623eeeb16539bd0d04efe44a026ba8
Deddy Romnan <romnandeddy@gmail.com>
2021-05-18 09:56:40 +0700

    add created_at, updated_at, and deleted_at timestamps

--- app/Database/Migrations/2021-05-11-192204_Obat.php
+++ app/Database/Migrations/2021-05-11-192204_Obat.php
@@ -46,6 +46,18 @@ class Obat extends Migration
 			'tgl_kedaluwarsa' => [
 				'type'			=> 'DATE',
 				'constraint'	=> null,
+			],
+			'created_at' => [
+				'type' => 'DATETIME',
+				'null' => true
+			],
+			'updated_at' => [
+				'type' => 'DATETIME',
+				'null' => true
+			],
+			'deleted_at' => [
+				'type' => 'DATETIME',
+				'null' => true
 			]
 		]);
 		$this->forge->addKey('id', true);
